947543a318df30f2fcef6b01a5820b105e2d3788
Abdullah Al Mamun <amamun@u.rochester.edu>
2020-11-09 20:28:01 -0500

    changed procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --bind 0.0.0.0:$PORT --pythonpath api api.app:app
+web: gunicorn --pythonpath api api.app:app
--- api/app.py
+++ api/app.py
@@ -217,4 +217,4 @@ def get_dist_tts():
 	return result
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=False)
+    app.run(debug=False)
